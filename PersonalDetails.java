bf97b2b2d5d5deba358ca6fbe90c7b8aab205dc3
Zoe Ang <zwAng@mcm.edu.ph>
2025-01-27 14:03:37 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,14 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+     String name = "Zoe Wednesday L. Ang";
+     int age = 18;
+     double height = 1.5;
+boolean isStudent = true;
+
+System.out.println("NAME: " + name);
+System.out.println("AGE: " + age);
+System.out.println("HEIGHT: " + height + " meters");
+System.out.println(" Is a Student? " + isStudent);
+
+    }
+}
